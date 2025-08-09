b18fcdf080c8024a3d34a955e88cd743f42f4748
William Fernandes <wiilfern1910@outlook.com>
2025-08-09 11:17:50 -0300

    feat: Setup Inicial Spring Initializer

---
+++ src/main/java/com/liamfer/Trenchat/TrenchatApplication.java
@@ -0,0 +1,13 @@
+package com.liamfer.Trenchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrenchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liamfer/Trenchat/TrenchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liamfer.Trenchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrenchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
