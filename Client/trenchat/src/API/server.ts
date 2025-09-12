f569112b65f16f752e34d9b5b8520276f54bef06
William Fernandes <wiilfern1910@outlook.com>
2025-09-12 14:47:19 -0300

    feat: Exit automático na Expiração do Token JWT

--- Client/trenchat/src/API/server.ts
+++ Client/trenchat/src/API/server.ts
@@ -5,3 +5,14 @@ export const serverApi = axios.create({
     baseURL: apiUrl || "http://192.168.101.69:3000",
     withCredentials: true,
 });
+
+// Cuidar da Expiração do Token JWT
+serverApi.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
